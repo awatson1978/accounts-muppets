fd46edc3efe4968722b5363aa9e7a90f09057eaf
Abigail Watson <awatson1978@gmail.com>
2013-12-04 16:03:49 -0500

    Update package.js

--- package.js
+++ package.js
@@ -3,11 +3,13 @@ Package.describe({
 });
 
 Package.on_use(function (api) {
+  api.use('accounts-base');
+
   api.add_files('bootstrap.users.js', 'server');
 
   api.add_files('muppets/Animal.jpg', "client", {isAsset: true});
   api.add_files('muppets/CrazyHarry.jpg', "client", {isAsset: true});
-  api.add_files('muppets/Dr.Honeydew.jpg', "client", {isAsset: true});
+  api.add_files('muppets/DrHoneydew.jpg', "client", {isAsset: true});
   api.add_files('muppets/Fozzy.jpg', "client", {isAsset: true});
   api.add_files('muppets/Gonzo.jpg', "client", {isAsset: true});
   api.add_files('muppets/Kermit.jpg', "client", {isAsset: true});
